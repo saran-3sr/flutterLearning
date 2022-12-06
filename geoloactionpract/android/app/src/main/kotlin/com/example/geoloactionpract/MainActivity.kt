208f4a6d15e6229ed9e5d9ecdd9c802dd8263bb0
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-12-06 17:48:26 +0530

    Initial commit

---
+++ geoloactionpract/android/app/src/main/kotlin/com/example/geoloactionpract/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geoloactionpract
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ geoloactionpract/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Position location = Position(
+      longitude: 0,
+      latitude: 0,
+      timestamp: DateTime(0),
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
+  Future getPosition() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.medium);
+    setState(() {
+      location = position;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Location Based on Your device GPS"),
+            Text('Altitude : ' + location.altitude.toString()),
+            Text('Longitude : ' + location.longitude.toString()),
+            Text('Latitude : ' + location.longitude.toString()),
+            TextButton(
+                onPressed: () {
+                  getPosition();
+                },
+                child: Text('Get Location'))
+          ],
+        ),
+      ),
+    );
+  }
+}
